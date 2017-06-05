20015c31e67cc566aee49e8400cd3ee744fde4f5
Alberto Rodríguez Tena <albertosss27@gmail.com>
2017-06-05 20:56:31 +0200

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-fd = open("/etc/passwd","r");
-lineas = fd.readlines()
-fd.close()
-
-for linea in lineas:
-    if not linea:
-        break
-    corte = linea.split(':')
-    login = corte[0] 
-    shell = corte[-1]
-    print login, shell[:-1]
-
+def multi(n,p):
+	return n*p
+def division(n,p):
+	return n/p
 def factorial(n):
-    return 1 if n < 1 else n * factorial(n-1)
+    	return 1 if n < 1 else n * factorial(n-1)
 def suma (c,p):
 	return c + p
 def resta(c,j):
 	return c - j
+def igual():
+	return '='
 
 	#jhedhgdchj
 class Test_EJER(unittest.TestCase):
-	def test_sa(self):
-        self.assertEqual(factorial(1),1)#aaaaaa
+	def test_factorial_1(self):
+        self.assertEqual(factorial(1),1)#de1
+	def test_factorial_2(self):
+	self.assertEqual(factorial(2),2)#de2
